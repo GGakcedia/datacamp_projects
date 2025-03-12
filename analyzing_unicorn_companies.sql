42e0b98bbe41360f4386c31f38973ae035c3f720
Özgür Kılıç <137221542+GGakcedia@users.noreply.github.com>
2025-03-12 06:29:09 +0300

    Add files via upload

---
+++ analyzing_unicorn_companies.sql
@@ -0,0 +1,43 @@
+"""You have been asked to support an investment firm by analyzing trends in 
+high-growth companies. They are interested in understanding which industries 
+are producing the highest valuations and the rate at which new high-value companies 
+are emerging. Providing them with this information gives them a competitive insight 
+as to industry trends and how they should structure their portfolio looking forward."""
+
+WITH top_industries AS
+(
+    SELECT i.industry, 
+        COUNT(i.*)
+    FROM industries AS i
+    INNER JOIN dates AS d
+        ON i.company_id = d.company_id
+    WHERE EXTRACT(year FROM d.date_joined) in ('2019', '2020', '2021')
+    GROUP BY industry
+    ORDER BY count DESC
+    LIMIT 3
+),
+
+yearly_rankings AS 
+(
+    SELECT COUNT(i.*) AS num_unicorns,
+        i.industry,
+        EXTRACT(year FROM d.date_joined) AS year,
+        AVG(f.valuation) AS average_valuation
+    FROM industries AS i
+    INNER JOIN dates AS d
+        ON i.company_id = d.company_id
+    INNER JOIN funding AS f
+        ON d.company_id = f.company_id
+    GROUP BY industry, year
+)
+
+SELECT industry,
+    year,
+    num_unicorns,
+    ROUND(AVG(average_valuation / 1000000000), 2) AS average_valuation_billions
+FROM yearly_rankings
+WHERE year in ('2019', '2020', '2021')
+    AND industry in (SELECT industry
+                    FROM top_industries)
+GROUP BY industry, num_unicorns, year
+ORDER BY year DESC, num_unicorns DESC
